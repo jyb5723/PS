9c353463614f85ede17e010af520d86ec9e79b5b
방진영 <proability@naver.com>
2018-09-20 04:37:49 +0900

    180920

--- boj10025.cpp
+++ boj10025.cpp
@@ -4,34 +4,31 @@
 #define N 51
 using namespace std;
 
-char a[N][N]; int n, m, vist[N][N];    // 0: 아무 단서도 안사용 1: 가로칸 단서로 사용   2: 세로칸 단서로 사용 
+char a[N][N]; int n, m, vist[N][N];   
 vector<pair<int, int>> v;
 
-bool r(int x, int y) {  // 해당칸이 가로단서가 되는지 확인
+bool r(int x, int y) { 
 	if (y + 2 >= m) return false;
 	if (a[x][y] == '#' || a[x][y + 1] == '#' || a[x][y + 2] == '#') return false;
-	if (y - 1 >= 0 && a[x][y - 1] != '#') return false;   // 경계를 벗어나지도 않으면서 왼쪽이 막혀있지도 않음 ( holy shat~ ) 
-														  //if (vist[x][y] == 1) return false;  // 가로단서로 사용됬었더라면
+	if (y - 1 >= 0 && a[x][y - 1] != '#') return false;  														 
 	return true;
 }
 
-bool c(int x, int y) {  // 해당칸이 세로단서가 되는지 확인 
+bool c(int x, int y) {  
 	if (x + 2 >= n) return false;
 	if (a[x][y] == '#' || a[x + 1][y] == '#' || a[x + 2][y] == '#') return false;
-	if (x - 1 >= 0 && a[x - 1][y] != '#') return false;   // 경계를 벗어나지도 않으면서 왼쪽이 막혀있지도 않음 ( holy shat~ ) 
-														  //if (vist[x][y] == 2) return false;  // 가로단서로 사용됬었더라면
+	if (x - 1 >= 0 && a[x - 1][y] != '#') return false;  														
 	return true;
 }
 
-void r_n(int x, int y) {  // 해당좌표로 부터 가로단서 처리
+void r_n(int x, int y) {  
 	while (y < m && a[x][y] == '.') {
 		vist[x][y] = 1;
 		y += 1;
 	}
 	return;
 }
-
-void c_n(int x, int y) {  // 해당좌표로부터 세로단서 처리 
+void c_n(int x, int y) { 
 	while (x < n && a[x][y] == '.') {
 		vist[x][y] = 2;
 		x += 1;
@@ -49,7 +46,7 @@ int main() {
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			bool res_c = c(i, j);  bool res_r = r(i, j); // 세로, 가로
+			bool res_c = c(i, j);  bool res_r = r(i, j); 
 			if (res_c || res_r) {
 				v.push_back({ i + 1, j + 1 });
 			}
--- boj10656.cpp
+++ boj10656.cpp
@@ -4,20 +4,20 @@
 #define N 51
 using namespace std;
 
-char a[N][N]; int n, m, vist[N][N];    // 0: 아무 단서도 안사용 1: 가로칸 단서로 사용   2: 세로칸 단서로 사용 
+char a[N][N]; int n, m, vist[N][N];   
 vector<pair<int, int>> v;
 
-bool r(int x, int y) {  // 해당칸이 가로단서가 되는지 확인
+bool r(int x, int y) {  
 	if (y + 2 >= m) return false;
 	if (a[x][y] == '#' || a[x][y + 1] == '#' || a[x][y + 2] == '#') return false;
-	if (y - 1 >= 0 && a[x][y - 1] != '#') return false;   // 경계를 벗어나지도 않으면서 왼쪽이 막혀있지도 않음 ( holy shat~ ) 												
+	if (y - 1 >= 0 && a[x][y - 1] != '#') return false;   												
 	return true;
 }
 
-bool c(int x, int y) {  // 해당칸이 세로단서가 되는지 확인 
+bool c(int x, int y) {  
 	if (x + 2 >= n) return false;
 	if (a[x][y] == '#' || a[x + 1][y] == '#' || a[x + 2][y] == '#') return false;
-	if (x - 1 >= 0 && a[x - 1][y] != '#') return false;   // 경계를 벗어나지도 않으면서 왼쪽이 막혀있지도 않음 ( holy shat~ ) 
+	if (x - 1 >= 0 && a[x - 1][y] != '#') return false;   
 	return true;
 }
 
@@ -30,7 +30,7 @@ int main() {
 			cin >> a[i][j];
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			bool res_c = c(i, j);  bool res_r = r(i, j); // 세로, 가로
+			bool res_c = c(i, j);  bool res_r = r(i, j); 
 			if (res_c || res_r) 
 				v.push_back({ i + 1, j + 1 });
 		}
