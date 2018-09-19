951690823772bf8f0e91b38a00946a09c1d8b15d
방진영 <proability@naver.com>
2018-09-20 04:59:23 +0900

    180920

--- boj10025.cpp
+++ boj10025.cpp
@@ -1,61 +1,35 @@
 #include<iostream>
-#include<vector>
 #include<algorithm>
-#define N 51
+
+#define N 100001
+#define M 1000001
+
 using namespace std;
 
-char a[N][N]; int n, m, vist[N][N];   
-vector<pair<int, int>> v;
-
-bool r(int x, int y) { 
-	if (y + 2 >= m) return false;
-	if (a[x][y] == '#' || a[x][y + 1] == '#' || a[x][y + 2] == '#') return false;
-	if (y - 1 >= 0 && a[x][y - 1] != '#') return false;  														 
-	return true;
-}
-
-bool c(int x, int y) {  
-	if (x + 2 >= n) return false;
-	if (a[x][y] == '#' || a[x + 1][y] == '#' || a[x + 2][y] == '#') return false;
-	if (x - 1 >= 0 && a[x - 1][y] != '#') return false;  														
-	return true;
-}
-
-void r_n(int x, int y) {  
-	while (y < m && a[x][y] == '.') {
-		vist[x][y] = 1;
-		y += 1;
-	}
-	return;
-}
-void c_n(int x, int y) { 
-	while (x < n && a[x][y] == '.') {
-		vist[x][y] = 2;
-		x += 1;
-	}
-	return;
-}
+int a[M], p_s[M], n, k, Max;
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	cin >> n >> m;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-			cin >> a[i][j];
-
+	cin >> n >> k;
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			bool res_c = c(i, j);  bool res_r = r(i, j); 
-			if (res_c || res_r) {
-				v.push_back({ i + 1, j + 1 });
-			}
-
-		}
+		int x, c;
+		cin >> c >> x;  // 물의 양과 좌표 
+		a[x] = c;
+		Max = max(x, Max);
 	}
-	cout << v.size() << '\n';
-	for (int i = 0; i < v.size(); i++)
-		cout << v[i].first << ' ' << v[i].second << '\n';
+	p_s[0] = a[0];
+	for (int i = 1; i <= Max; i++)
+		p_s[i] = a[i] + p_s[i - 1];
 
+	int ans = 0;
+	for (int i = 0; i <= Max; i++) {
+		int Sum = 0;
+		if (i + k <= Max) Sum += p_s[i + k];
+		else if (i + k > Max) Sum += p_s[Max];
+		if (i - k > 0) Sum -= p_s[i - k - 1];
+		ans = max(Sum, ans);
+	}
+	cout << ans;
 	return 0;
 }
