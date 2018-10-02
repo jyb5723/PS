f2cf7eeee37ebf59c73616f51060fa0297573af8
방진영 <proability@naver.com>
2018-10-02 16:27:55 +0900

    181002

---
+++ CodeForce/#511_DIV2(A).cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std; 
+
+
+int main() {
+	ios::sync_with_stdio(false); 
+	cin.tie(NULL), cout.tie(NULL); 
+	int t, n, k; cin >> t; 
+	k = t % 3; 
+	if (!k || k == 1) {
+		cout << "1 " << "1 " << t - 2 << '\n'; 
+	}
+	else if (k == 2) {
+		cout << "1 " << "2 " << t - 3 << '\n'; 
+	}
+
+	return 0; 
+}
